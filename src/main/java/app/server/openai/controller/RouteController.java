70acfed8181e178c58bcf5a9a53a84488470cd67
wook <goodbyeyo@naver.com>
2025-05-20 23:59:58 +0900

    create thymeleaf view page, router

---
+++ src/main/java/app/server/openai/controller/RouteController.java
@@ -0,0 +1,14 @@
+package app.server.openai.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RouteController {
+
+    @GetMapping("/ask_view")
+    public String askView() {
+        return "ask";
+    }
+}
